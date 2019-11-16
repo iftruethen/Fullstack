3246864c7ed04088a365f0b6b0715d93a03d779c
iftruedo <jere.ahkiola@gmail.com>
2019-11-16 18:32:19 +0200

    E2_14

--- Osa2/Excercise2_12/src/App.js
+++ Osa2/Excercise2_12/src/App.js
@@ -5,35 +5,51 @@ const App = () => {
   const [ countries,setCountries ] = useState([])
   const [ showAll, setShowAll ] = useState(false)
   const [ searchWord, setSearchWord ] = useState('')
+  const [ weatherData, setWeatherData ] = useState([])
 
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => setCountries(response.data))
-  }, [])
+    axios
+      .get('http://api.weatherstack.com/current?access_key=4cf035647c4a0a31ae72f44ebb1aab2f&query='.concat(searchWord))
+      .then(response => setWeatherData(response.data))
+  }, [searchWord])
 
-  const RenderCountry = ({match}) => {
-    return(
-      <div>
-        <h2>{match.name}</h2>
-        <p>
-          capital {match.capital} <br/>
-          population {match.population}
-        </p>
-        <h3>languages</h3>
-        <ul>
-          {match.languages.map(language => <li>{language.name}</li>)}
-        </ul>
-        <img src={match.flag} width="10%" height="10%"></img>
-      </div>
-    )
+  const renderCountry = ({match}) => {
+    var weather = []
+    try {
+      weather = weatherData.current
+    } catch(err) {}
+
+    if (weather !== undefined) {
+      return(
+        <div>
+          <h2>{match.name}</h2>
+          <p>
+            capital {match.capital} <br/>
+            population {match.population}
+          </p>
+          <h3>languages</h3>
+          <ul>
+            {match.languages.map(language => <li>{language.name}</li>)}
+          </ul>
+          <img src={match.flag} width="10%" height="10%"></img>
+          <h3>Weather in {match.capital}</h3>
+          <p><b>temperature:</b> {weather.temperature} Celsius</p>
+          <img src={weather.weather_icons[0]}></img>
+          <p><b>wind:</b> {weather.wind_speed} kph direction {weather.wind_dir} </p>
+        </div>
+      )
+    } else {
+      return null
+    }
   }
 
   const renderer = () => {
     var countriesToShow = showAll
     ? countries
     : countries.filter(country => country.name.toLowerCase().includes(searchWord))
-    console.log({countriesToShow})
 
     if (countriesToShow.length > 10) {
       return(
@@ -50,7 +66,7 @@ const App = () => {
     } else if (countriesToShow.length === 1) {
       const match = countriesToShow[0]
       return(
-        RenderCountry({match})
+        renderCountry({match})
       )
     } else {
       return(
